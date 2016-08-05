e2490ffaa296dafa238c7a9f359be6942e2d6765
Elisabeth Gross <elisabethgross568@gmail.com>
2016-08-04 20:51:44 -0400

    idk

--- browser/js/common/directives/rando-greeting/rando-greeting.js
+++ browser/js/common/directives/rando-greeting/rando-greeting.js
@@ -2,10 +2,7 @@ app.directive('randoGreeting', function (RandomGreetings) {
 
     return {
         restrict: 'E',
-        templateUrl: 'js/common/directives/rando-greeting/rando-greeting.html',
-        link: function (scope) {
-            scope.greeting = RandomGreetings.getRandomGreeting();
-        }
+        templateUrl: 'js/common/directives/rando-greeting/rando-greeting.html'
     };
 
 });
