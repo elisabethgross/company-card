57a91dcc182e83359d732b4709f02d46fbac32bb
Elisabeth Gross <elisabethgross568@gmail.com>
2016-08-08 10:57:12 -0400

    hello

--- server/app/routes/cards/index.js
+++ server/app/routes/cards/index.js
@@ -1,5 +1,14 @@
 var router = require('express').Router();
 var Card = require('../../../db/models/card');
+var braintree = require('braintree');
+
+var gateway = braintree.connect({
+    environment: braintree.Environment.Sandbox,
+    merchantId: "jp3ytdqkpbfsgcn6",
+    publicKey: "c63w2pdxhz6hgthc",
+    privateKey: "d6adf97f9b029e741b2bad38a48143d7"
+});
+
 module.exports = router;
 
 router.param('id', function(req, res, next, id) {
@@ -15,6 +24,12 @@ router.param('id', function(req, res, next, id) {
   .catch(next);
 });
 
+// router.get('/checkout', function (req, res) {
+//     gateway.clientToken.generate({}, function (err, res) {
+//        res.json({clientToken: res.clientToken});
+//     });
+// });
+
 router.get('/', function (req, res, next) {
   Card.findAll({})
   .then(function (cards) {
@@ -28,6 +43,20 @@ router.get('/:id', function (req, res) {
 });
 
 router.post('/', function (req, res, next) {
+  var limit = req.body.limit.slice(1, req.body.limit.length-1);
+  gateway.transaction.sale({
+    amount: limit,
+    customer: {
+      firstName: req.body.name,
+    },
+    paymentMethodNonce: "fake-valid-nonce",
+    options: {
+      submitForSettlement: true
+    }
+  }, function (err, result) {
+    if (err) console.log(err);
+    else console.log(result);
+  });
   Card.create(req.body)
   .then(function (card) {
     res.send(card);
